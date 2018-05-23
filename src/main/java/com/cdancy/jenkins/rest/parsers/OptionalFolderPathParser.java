adebd004dd1e7c5c2b8c2ce6a6639e10ef0d9926
Dhivyaa Nandakumar <dhivyaa89@gmail.com>
2018-05-23 13:24:30 -0400

    Used StringBuilder for string manipulation

--- src/main/java/com/cdancy/jenkins/rest/parsers/OptionalFolderPathParser.java
+++ src/main/java/com/cdancy/jenkins/rest/parsers/OptionalFolderPathParser.java
@@ -6,33 +6,33 @@ import com.google.common.base.Function;
 import javax.inject.Singleton;
 
 /*
- * Turn the optionalFolderPath param to jenkins URL style if needed
+ * Turn the optionalFolderPath param to jenkins URL style
  */
 @Singleton
 public class OptionalFolderPathParser implements Function<Object,String> {
 
     public static final String EMPTY_STRING = "";
+
     @Override
     public String apply(Object optionalFolderPath) {
         if(optionalFolderPath == null) {
             return EMPTY_STRING;
         }
 
-        String folderPath = String.class.cast(optionalFolderPath);
-        String amendedPath = "";
+        StringBuilder path = new StringBuilder(String.class.cast(optionalFolderPath));
+        StringBuilder amendedPath = new StringBuilder();
 
-        if(folderPath.startsWith("/")) {
-            folderPath = folderPath.replaceAll("^/+","");
+        if(path.charAt(0) == '/'){
+            path.deleteCharAt(0);
+        }
+        if(path.charAt(path.length()-1) == '/') {
+            path.deleteCharAt(path.length()-1);
         }
 
-        if(folderPath.endsWith("/")) {
-            folderPath = folderPath.replaceAll("/+$","");
+        String[] folders = path.toString().split("/");
+        for(String folder:folders) {
+            amendedPath.append("job/").append(folder).append("/");
         }
-
-        String[] folderNames = folderPath.split("/");
-        for (String folder:folderNames) {
-            amendedPath += "job/" + folder + "/";
-        }
-        return amendedPath;
+        return amendedPath.toString();
     }
 }
