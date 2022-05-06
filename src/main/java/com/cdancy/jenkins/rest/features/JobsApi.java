d0d6c5d1e6d17e825d68687f7baf3243f307f3b9
宋汉林 <songhanlin@qianmi.com>
2022-05-06 09:13:09 +0800

    解决修改配置导致的中文乱码问题

--- src/main/java/com/cdancy/jenkins/rest/features/JobsApi.java
+++ src/main/java/com/cdancy/jenkins/rest/features/JobsApi.java
@@ -100,7 +100,7 @@ public interface JobsApi {
     @Named("jobs:update-config")
     @Path("{optionalFolderPath}job/{name}/config.xml")
     @Fallback(Fallbacks.FalseOnNotFoundOr404.class)
-    @Produces(MediaType.APPLICATION_XML)
+    @Produces({MediaType.APPLICATION_XML, "text/html; charset=UTF-8"})
     @Consumes(MediaType.TEXT_HTML)
     @Payload("{configXML}")
     @POST
